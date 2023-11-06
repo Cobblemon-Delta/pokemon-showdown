ad4789271b9d0ac05c2d5da5e868ccf493cb5587
Cody Spratford <koreanpanda345@gmail.com>
2023-11-05 19:18:18 -0800

    Quaxolotl's Evolution Lines tiers

--- data/mods/cobblemon/formats-data.ts
+++ data/mods/cobblemon/formats-data.ts
@@ -1,3 +1,10 @@
 export const FormatsData: {[specieid: string]: ModdedSpeciesFormatsData} = {
-
+	wooperdelta: {
+		isNonstandard: "Past",
+		natDexTier: "LC",
+	},
+	quaxolotl: {
+		isNonstandard: "Past",
+		natDexTier: "OU",
+	},
 };
