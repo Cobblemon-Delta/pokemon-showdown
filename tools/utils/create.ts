c9b85384170818bf065abecb58bfe5679483e351
Cody Spratford <koreanpanda345@gmail.com>
2023-11-07 14:24:29 -0800

    Update create.ts

--- tools/utils/create.ts
+++ tools/utils/create.ts
@@ -9,30 +9,30 @@ export class CreatePokemonLearnset {
 
 	private createMoveSection(name: string) {
 		console.log(this.dex.modData("Learnsets", this.name));
-		if (this.dex.modData("Learnsets", this.name).learnsets == undefined) {
-			this.dex.modData("Learnsets", this.name).learnsets = {};
+		if (this.dex.modData("Learnsets", this.name).learnset == undefined) {
+			this.dex.modData("Learnsets", this.name).learnset = {};
 		}
-		if(this.dex.modData("Learnsets", this.name).learnsets[name] == undefined)
-			this.dex.modData("Learnsets", this.name).learnsets[name] = [];
+		if(this.dex.modData("Learnsets", this.name).learnset[name] == undefined)
+			this.dex.modData("Learnsets", this.name).learnset[name] = [];
 	}
 	public addLevelMove(name: string, level: number = 1) {
 		this.createMoveSection(name);
-		this.dex.modData("Learnsets", this.name).learnsets[name].push(`9L${level}`);
+		this.dex.modData("Learnsets", this.name).learnset[name].push(`9L${level}`);
 		return this;
 	}
 	public addTMMove(name: string) {
 		this.createMoveSection(name);
-		this.dex.modData("Learnsets", this.name).learnsets[name].push(`9M`);
+		this.dex.modData("Learnsets", this.name).learnset[name].push(`9M`);
 		return this;
 	}
 	public addEggMove(name: string) {
 		this.createMoveSection(name);
-		this.dex.modData("Learnsets", this.name).learnsets[name].push(`9E`);
+		this.dex.modData("Learnsets", this.name).learnset[name].push(`9E`);
 		return this;
 	}
 	public addTutorMove(name: string) {
 		this.createMoveSection(name);
-		this.dex.modData("Learnsets", this.name).learnsets[name].push(`9T`);
+		this.dex.modData("Learnsets", this.name).learnset[name].push(`9T`);
 		return this;
 	}
 }
