88135e79e84603afa9c1ec8e7162396b7a9c09e7
Cody Spratford <koreanpanda345@gmail.com>
2023-11-08 12:38:26 -0800

    Testing

--- data/mods/cobblemon/learnsets/quaxolotl.ts
+++ data/mods/cobblemon/learnsets/quaxolotl.ts
@@ -6,6 +6,19 @@ export function QuaxolotlLearnset(dex: ModdedDex) {
 		// Level Up Moves
 		////////////////////////////////////////
 		pokemon
+			.addLevelMove("Water Gun", 1)
+			.addLevelMove("Tail Whip", 1)
+			.addLevelMove("Rain Dance", 4)
+			.addLevelMove("Disarming Voice", 8)
+			.addLevelMove("Mist", 12)
+			.addLevelMove("Haze", 12)
+			.addLevelMove("Slam", 16)
+			.addLevelMove("Yawn", 23)
+			.addLevelMove("Aqua Tail", 28)
+			.addLevelMove("Muddy Water", 34)
+			.addLevelMove("Charm", 40)
+			.addLevelMove("Play Rough", 46)
+			.addLevelMove("Moonblast", 52);
 		/////////////////////////////////////////
 		// TM/HM Moves
 		////////////////////////////////////////
--- tools/utils/create.ts
+++ tools/utils/create.ts
@@ -6,9 +6,11 @@ export class CreatePokemon {
 export class CreatePokemonLearnset {
 	
 	constructor(private name: string, private dex: ModdedDex) {}
-
+	private PrefixMoveName(name: string) {
+		return name.includes(" ") ? name.toLowerCase().replace(" ", "") : name.toLowerCase();
+	}
 	private createMoveSection(name: string) {
-		console.log(this.dex.modData("Learnsets", this.name));
+		name = this.PrefixMoveName(name);
 		if (this.dex.modData("Learnsets", this.name).learnset == undefined) {
 			this.dex.modData("Learnsets", this.name).learnset = {};
 		}
@@ -16,21 +18,25 @@ export class CreatePokemonLearnset {
 			this.dex.modData("Learnsets", this.name).learnset[name] = [];
 	}
 	public addLevelMove(name: string, level: number = 1) {
+		name = this.PrefixMoveName(name);
 		this.createMoveSection(name);
 		this.dex.modData("Learnsets", this.name).learnset[name].push(`9L${level}`);
 		return this;
 	}
 	public addTMMove(name: string) {
+		name = this.PrefixMoveName(name);
 		this.createMoveSection(name);
 		this.dex.modData("Learnsets", this.name).learnset[name].push(`9M`);
 		return this;
 	}
 	public addEggMove(name: string) {
+		name = this.PrefixMoveName(name);
 		this.createMoveSection(name);
 		this.dex.modData("Learnsets", this.name).learnset[name].push(`9E`);
 		return this;
 	}
 	public addTutorMove(name: string) {
+		name = this.PrefixMoveName(name);
 		this.createMoveSection(name);
 		this.dex.modData("Learnsets", this.name).learnset[name].push(`9T`);
 		return this;
