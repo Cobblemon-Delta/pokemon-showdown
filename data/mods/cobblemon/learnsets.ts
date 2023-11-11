6d9084ba9254fb83022c1abd18b383fbdbe8fb01
Cody Spratford <koreanpanda345@gmail.com>
2023-11-10 17:04:50 -0800

    Update learnsets.ts

--- data/mods/cobblemon/learnsets.ts
+++ data/mods/cobblemon/learnsets.ts
@@ -1,62 +1,17 @@
 export const Learnsets: {[k: string]: ModdedLearnsetData} = {
-	spectreon: {
-		learnset: {
-			
-		},
-	},
-	wooperdelta: {
-		learnset: {
-
-		}
-	},
-	quaxolotl: {
-		learnset: {
-
-		}
-	},
-	lucaurus: {
-		learnset: {
-
-		}
-	},
-	sableyedelta: {
-		learnset: {
-
-		}
-	},
-	yanfern: {
-		learnset: {
-
-		}
-	},
-	yanmellia: {
-		learnset: {
-
-		}
-	},
-	gibledelta: {
-		learnset: {}
-	},
-	gabitedelta: {
-		learnset: {}
-	},
-	garchompdelta: {
-		learnset: {}
-	},
-	gorochu: {
-		learnset: {}
-	},
-	greninjadelta: {
-		learnset: {}
-	},
-	raticatedelta: {
-		learnset: {}
-	},
-	rattatadelta: {
-		learnset: {}
-	},
-	steelixdelta: {
-		learnset: {}
-	},
-
+	gabitedelta: {learnset: {}},
+	garchompdelta: {learnset: {}},
+	gibledelta: {learnset: {}},
+	gorochu: {learnset: {}},
+	greninjadelta: {learnset: {}},
+	lucaurus: {learnset: {}},
+	quaxolotl: {learnset: {}},
+	raticatedelta: {learnset: {}},
+	rattatadelta: {learnset: {}},
+	sableyedelta: {learnset: {}},
+	spectreon: {learnset: {}},
+	steelixdelta: {learnset: {}},
+	wooperdelta: {learnset: {}},
+	yanfern: {learnset: {}},
+	yanmellia: {learnset: {}},
 }
