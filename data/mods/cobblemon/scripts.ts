82ff6ae91596e64195454ac1b539f48f465d0f2e
Cody Spratford <koreanpanda345@gmail.com>
2024-02-03 16:10:28 -0800

    Go back to the basic file structure for learnsets

--- data/mods/cobblemon/scripts.ts
+++ data/mods/cobblemon/scripts.ts
@@ -2,11 +2,4 @@ import { LoadLearnsets } from "./learnsets/index";
 export const Scripts: ModdedBattleScriptsData = {
 	gen: 9,
 	inherit: "gen9",
-	init() {
-		//#region Learnset
-		(async () => {
-			await LoadLearnsets(this);
-		})()
-		//#endregion
-	},
 };
