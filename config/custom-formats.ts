e537eda973281303abc3b4e15e766d84ffee9e16
Cody Spratford <koreanpanda345@gmail.com>
2024-02-03 17:11:15 -0800

    Moving to Natdex

--- config/custom-formats.ts
+++ config/custom-formats.ts
@@ -9,7 +9,7 @@ export const Formats: FormatList = [
 		name: "[Gen 9] National Dex Single Battle",
 		desc: "Cobblemon Single Battles",
 		mod: 'cobblemon',
-		ruleset: ['Standard', 'Cobblemon Pokedex'],
+		ruleset: ['Standard NatDex', 'OHKO Clause', 'Evasion Clause', 'Species Clause', 'Sleep Clause Mod'],
 		banlist: ['Uber', 'AG', 'Arena Trap', 'Moody', 'Sand Veil', 'Shadow Tag', 'Snow Cloak', 'King\'s Rock', 'Razor Fang', 'Baton Pass', 'Last Respects', 'Shed Tail'],
 	}
 ];
--- data/mods/cobblemon/formats-data.ts
+++ data/mods/cobblemon/formats-data.ts
@@ -1,17 +1,26 @@
 export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 	aegislashdelta: {
+		isNonstandard: "Past",
 		tier: "OU",
+		natDexTier: "OU",
 	},
 	wooperdelta: {
+		isNonstandard: "Past",
 		tier: "LC",
+		natDexTier: "LC",
 	},
 	quaxolotl: {
+		isNonstandard: "Past",
 		tier: "OU",
+		natDexTier: "OU",
 	},
 	spectreon: {
+		isNonstandard: "Past",
 		tier: "OU",
+		natDexTier: "OU",
 	},
 	dragetsu: {
+		isNonstandard: "Past",
 		tier: "OU",
 		natDexTier: "OU",
 	}
